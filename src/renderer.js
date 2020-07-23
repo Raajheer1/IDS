2ece07750e34eca37f969f9f719dbf1c59bfb716
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2020-07-22 21:07:12 -0500

    Added the Top Menu

---
+++ src/renderer.js
@@ -0,0 +1,9 @@
+// const customTitlebar = require('custom-electron-titlebar');
+// let IDSTitleBar = new customTitlebar.Titlebar({
+//     backgroundColor: customTitlebar.Color.fromHex("#242322"),
+//     shadow: true,
+//     icon: "./img/icon.png"
+// });
+
+// IDSTitleBar.updateTitle('ZDVARTCC');
+// IDSTitleBar.titleHorizontalAlignment("center");
