d1489c3fc919032f83a7959506216109c52e401d
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2020-07-23 19:06:04 -0500

    Removed useless comments

--- src/renderer.js
+++
@@ -1,9 +0,0 @@
-// const customTitlebar = require('custom-electron-titlebar');
-// let IDSTitleBar = new customTitlebar.Titlebar({
-//     backgroundColor: customTitlebar.Color.fromHex("#242322"),
-//     shadow: true,
-//     icon: "./img/icon.png"
-// });
-
-// IDSTitleBar.updateTitle('ZDVARTCC');
-// IDSTitleBar.titleHorizontalAlignment("center");
