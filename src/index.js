0bf004365bdfe721b9c15f59ff201315180ff04d
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-01-06 01:11:26 -0600

    Updated more stuff

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const createWindow = () => {
     height:1080,
     frame: false,
     titleBarStyle: "hidden",
-    icon:__dirname+"/img/icon.png",
+    icon:__dirname+"/img/BigIcon.png",
     webPreferences: {
       nodeIntegration: true
     }
