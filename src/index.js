0203a9d3ddb0a44d226fbb2eec41b57e5e3bc31e
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-02-06 00:53:07 -0600

    0.2.1

--- src/index.js
+++ src/index.js
@@ -117,7 +117,12 @@ ipcMain.on('specialist', (event, arg) => {
 });
 
 
-
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  mainWindow.webContents.send('download-progress', log_message);
+})
 
 autoUpdater.on('update-available', () => {
   mainWindow.webContents.send('update_available');
