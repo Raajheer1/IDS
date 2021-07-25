6a3a69a27bd7b51a8a2e09f76f43ef2c7863eb97
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-07-25 12:13:42 -0500

    Version 1.0 - Updated IDS URL

--- src/WebSocket.js
+++ src/WebSocket.js
@@ -6,7 +6,7 @@
    it under the terms of the GNU Affero General Public License as published
    by the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -71,7 +71,7 @@ function init() {
     ws.close();
     }
 
-    ws = new WebSocket('wss://iws.denartcc.org:8080');
+    ws = new WebSocket('wss://iws.denartcc.org:7050');
 
     ws.onopen = () => {
     console.log("WS Connection opened!");
